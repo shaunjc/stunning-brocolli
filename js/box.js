28da1657e0d4993fdb8675ee62b3fcb6757e67dc
Shaun Cockerill <shaun@slickdesign.com.au>
2019-02-08 11:06:00 +0800

    Additional open and close event listeners with new classes to match.

--- js/box.js
+++ js/box.js
@@ -1,8 +1,8 @@
 if (window.jQuery === void(0)) {
-    var boxes = document.getElementsByClassName('box-die');
-    for (var i in boxes) {
-        if (boxes.hasOwnProperty(i)) {
-            boxes[i].addEventListener('click', function(){
+    var clickToggle = document.getElementsByClassName('js-click-toggle-open');
+    for (var i in clickToggle) {
+        if (clickToggle.hasOwnProperty(i)) {
+            clickToggle[i].addEventListener('click', function(){
                 if(this.classList.contains('open')) {
                     this.classList.remove('open');
                 }
@@ -12,11 +12,52 @@ if (window.jQuery === void(0)) {
             });
         }
     }
+    
+    var enterOpen = document.getElementsByClassName('js-enter-open');
+    for (var i in enterOpen) {
+        if (enterOpen.hasOwnProperty(i)) {
+            enterOpen[i].addEventListener('mouseenter', function(){
+                this.classList.add('open');
+            });
+        }
+    }
+    var leaveOpen = document.getElementsByClassName('js-leave-open');
+    for (var i in leaveOpen) {
+        if (leaveOpen.hasOwnProperty(i)) {
+            leaveOpen[i].addEventListener('mouseenter', function(){
+                this.classList.add('open');
+            });
+        }
+    }
+    var enterClose = document.getElementsByClassName('js-enter-close');
+    for (var i in enterClose) {
+        if (enterClose.hasOwnProperty(i)) {
+            enterClose[i].addEventListener('mouseleave', function(){
+                this.classList.remove('open');
+            });
+        }
+    }
+    var leaveClose = document.getElementsByClassName('js-leave-close');
+    for (var i in leaveClose) {
+        if (leaveClose.hasOwnProperty(i)) {
+            leaveClose[i].addEventListener('mouseleave', function(){
+                this.classList.remove('open');
+            });
+        }
+    }
 }
 else {
     jQuery(function($){
-        $('body').on('click', '.box-die', function() {
+        $('body').on('click', '.js-click-toggle-open', function(){
             $(this).toggleClass('open');
+        }).on('mouseenter', '.js-enter-open', function(){
+            $(this).addClass('open');
+        }).on('mouseleave', '.js-leave-open', function(){
+            $(this).addClass('open');
+        }).on('mouseenter', '.js-enter-close', function(){
+            $(this).removeClass('open');
+        }).on('mouseleave', '.js-leave-close', function(){
+            $(this).removeClass('open');
         });
     });
 }
